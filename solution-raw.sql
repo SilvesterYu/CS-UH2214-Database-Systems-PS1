ca0d16cfafc79ca8fe412bb9ca6d7f8b5ef558f6
Silvester Yu <ly1164@nyu.edu>
2022-02-23 08:51:11 +0400

    query on raw data

---
+++ solution-raw.sql
@@ -0,0 +1,10 @@
+-- Question1
+/*select pub.p, count(*)
+from pub
+group by pub.p*/
+-- (article, 2783058) (book, 19254) (incollection, 67674) (inproceedings, 2984445)
+
+-- Question2
+
+		  
+							  )
