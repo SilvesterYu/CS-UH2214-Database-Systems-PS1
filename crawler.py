df6ce378bc67f5d5195e48e2f900274f1221dfe6
Silvester Yu <ly1164@nyu.edu>
2022-02-23 21:54:28 +0400

    completed crawler func

--- crawler.py
+++ crawler.py
@@ -10,35 +10,50 @@ import requests #a library to load web pages
 import pandas as pd #import pandas library to help us store the data
 import numpy as np
 
-data = []
-base_url = "http://portal.core.edu.au/conf-ranks/?search=&by=all&source=all&sort=atitle&page="
+####################### a simple crawler ##############################
+def crawler_func(start_page, end_page, base_url):
+    data = []
+    
+    for i in range(start_page, end_page+1):
+        print("--------------------", i, "--------------------------")
+        my_url = base_url + str(i)
+        r = requests.get(my_url)
+        soup = bs(r.content, features="html.parser")        
+        soup.prettify() #on Google Colab it will not show the HTML with indentations but if you did this in Python directly, it will work correctly.
+        rows = soup.find_all('tr', {"class": ["evenrow", "oddrow"]})
+        for row in rows:
+          line = [] # store all things in one line
+          cells = row.find_all("td")
+          for cell in cells:
+            clean_row = cell.text.strip('\n').strip()
+            line.append(clean_row)
+          if row.find("a") != None:
+            line[4] = row.find("a")["href"]
+          data.append(line)
+          #print(line)
+    np_data = np.array(data)
+    df = pd.DataFrame(np_data)
+    return df
 
-for i in range(1, 46):
-    print("--------------------", i, "--------------------------")
-    my_url = base_url + str(i)
-    r = requests.get(my_url)
-    soup = bs(r.content, features="html.parser")
+########################### for conference ########################
+base_url1 = "http://portal.core.edu.au/conf-ranks/?search=&by=all&source=all&sort=atitle&page="
+start_page1 = 1
+end_page1 = 45
+
+df1 = crawler_func(start_page1, end_page1, base_url1)
+
+df1.columns = ['title', 'acronym', 'source', 'rank', 'dblp', 'hasdata', 'primaryfor', 'comments', 'averagerating']
+df1 = df1[['title', 'acronym', 'source', 'rank', 'dblp']]    
+
+########################## for journal ########################
+base_url2 = "http://portal.core.edu.au/conf-ranks/?search=&by=all&source=all&sort=atitle&page="
+start_page2 = 1
+end_page2 = 18
+
+df2 = crawler_func(start_page2, end_page2, base_url2)
+
+df2.columns = ['title', 'acronym', 'source', 'rank', 'dblp', 'hasdata', 'primaryfor', 'comments', 'averagerating']
+df2 = df2[['title', 'acronym', 'source', 'rank', 'dblp']]  
     
-    soup.prettify() #on Google Colab it will not show the HTML with indentations but if you did this in Python directly, it will work correctly.
-    rows = soup.find_all('tr', {"class": ["evenrow", "oddrow"]})
-    for row in rows:
-      line = [] # store all things in one line
-      cells = row.find_all("td")
-      for cell in cells:
-        clean_row = cell.text.strip('\n').strip()
-        line.append(clean_row)
-      if row.find("a") != None:
-        line[4] = row.find("a")["href"]
-      data.append(line)
-      print(line)
-      
-print(data[:200])
-print(len(data))
-    
-np_data = np.array(data)
-df = pd.DataFrame(np_data)
-    
-    
-    
-    
+
     
