0544c9148881db6284d7e4a499a93845eab3b9eb
Silvester Yu <ly1164@nyu.edu>
2022-02-23 11:58:03 +0400

    latest ver

--- solution-raw.sql
+++ solution-raw.sql
@@ -1,10 +1,18 @@
 -- Question1
-/*select pub.p, count(*)
+select pub.p, count(*)
 from pub
-group by pub.p*/
+group by pub.p;
+
 -- (article, 2783058) (book, 19254) (incollection, 67674) (inproceedings, 2984445)
 
 -- Question2
+select fp from
+(select distinct field.p as fp, pub.p as pp from field, pub where field.k = pub.k) as temp1
+group by fp
+having count(*) >= 8;
+-- "author", "ee", "note", "title", "year"
 
-		  
-							  )
+-- Question 3:
+
+create index pub_idx on pub (k, p);
+create index field_idx on field(k, p, i, v);
